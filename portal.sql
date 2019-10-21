7e7b2827735d420a1c8d04e9c31e7563c193f75c
Mateusz Murawski <matimaxi@o2.pl>
2019-10-21 02:16:14 +0200

    posty

---
+++ portal.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 21 Paź 2019, 02:15
+-- Wersja serwera: 10.1.21-MariaDB
+-- Wersja PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `posty`
+--
+
+CREATE TABLE `posty` (
+  `ID` int(11) NOT NULL,
+  `ID_LOGIN` int(11) NOT NULL,
+  `TEKST` text COLLATE utf8_polish_ci NOT NULL,
+  `DATA` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `posty`
+--
+
+INSERT INTO `posty` (`ID`, `ID_LOGIN`, `TEKST`, `DATA`) VALUES
+(8, 40, 'afdf sadf asd f as f asdf as df as df asd f\r\nasdf asd fs adf ads f asdf \r\nsdf as f asdf sad f sa f \r\ndf asd fsad f saddf s adf\r\nas df sadf sad f sdaf s adf\r\nfasdf asd f dsaf sda f\r\nsf ads fds af asf ', '2019-10-21 02:07:33'),
+(10, 41, 'sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf ', '2019-10-21 02:11:49'),
+(11, 41, 'sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf ', '2019-10-21 02:12:20'),
+(12, 41, 'sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf ', '2019-10-21 02:13:08'),
+(13, 41, 'sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf ', '2019-10-21 02:13:38'),
+(14, 41, 'sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf sdf asd f asdfsadfasd ffads fsad fas fsad fsad fasd sad ffsafsasd fasd fasdfassdafadsfsadfsaddasdafsa fas f saf sa df as fs af sd f sa df sdaf ds f sd fa sf sa f sa fasdf ', '2019-10-21 02:13:48');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `ID` int(11) NOT NULL,
+  `LOGIN` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `HASLO` varchar(254) COLLATE utf8_polish_ci NOT NULL,
+  `EMAIL` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `URODZENIE` date NOT NULL,
+  `PLEC` varchar(10) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`ID`, `LOGIN`, `HASLO`, `EMAIL`, `URODZENIE`, `PLEC`) VALUES
+(40, 'mateusz', '$2y$10$MsR2sNzj4VHTAXEjZmRvwuH3cKk.QJIRs8eUgrg2LW7frlBGRwc6i', 'mat@o2.pl', '2019-10-01', 'M'),
+(41, 'bartek', '$2y$10$gxrGmHqNtnqqded2T4G4Be2sGzVd39oh9j70BDWRERMlp37v/J.tm', 'bartek@o2.pl', '2019-10-05', 'M'),
+(42, 'Natalia', '$2y$10$DgRJkMEtGpqpRUT8kr10o.4bbpAsqy/8q/f10IkzlLrcgpmCRHfdi', 'natalia@o2.pl', '2019-10-01', 'M');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `posty`
+--
+ALTER TABLE `posty`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `LOGIN` (`LOGIN`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `posty`
+--
+ALTER TABLE `posty`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- tablica.php
+++ tablica.php
@@ -1,14 +1,4 @@
-<!doctype html>
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <title>Witaj</title>
-            <link rel="stylesheet" href="style.css">
-            <link rel="stylesheet" href="tablica.css">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-            <meta name="description" content="Witaj" />
-
-            <?php 
+<?php 
 
                 session_start();
 
@@ -23,7 +13,59 @@
                     unset($_SESSION['zalogowany-user']);
                     header('Location:logowanie.php');
                 }
+
+                if(isset($_POST['post']))
+                {
+                    $post = strip_tags($_POST["post"]); 
+                    $login = $_SESSION['zalogowany-user'];
+
+                    require_once "polaczenie.php";
+                    mysqli_report(MYSQLI_REPORT_STRICT);
+    
+                    try
+                    {
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        if ($conn->connect_errno!=0)
+                            {
+                                throw new Exception(mysqli_connect_errno());
+                            }
+                        else
+                        {
+                            if($rezultat = $conn->query("INSERT INTO posty (ID_LOGIN, TEKST, DATA)
+                            VALUES ((SELECT ID FROM uzytkownicy WHERE LOGIN='$login'), '$post', CURRENT_TIMESTAMP())"))
+                            {
+                                echo "post dodany";
+                            }
+                            
+                            else
+                            {
+                                throw new Exception($conn->error);
+                            }
+
+                            $conn->close();
+                        }
+                    }                  
+ 
+                    catch(Expection $e)
+                    {
+                        echo '<span style="color:red;"> Błąd połaczenia! Przepraszamy! Proszę spróbować za chwilę!</span>';
+                    }
+
+                }
             ?>
+
+
+<!doctype html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Witaj</title>
+            <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="tablica.css">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+            <meta name="description" content="Witaj" />
+
+            
 
         </head>
 
@@ -45,6 +87,7 @@
                             </div>
 
                         </div>
+                        <div class="pasek_pole"> </div>
                         <div class="log">
                             <div class="x"></div>
                             <div class="y">
@@ -53,46 +96,78 @@
                                 </div>
 
                                 <div class="post">
-                                    <textarea placeholder="Napisz nowy post..."  name="post" class="okno_post"></textarea>
+                                    <form action="tablica.php" method="post">
+                                        <textarea placeholder="Napisz nowy post..."  name="post" class="okno_post"></textarea>
                                 </div>
 
                                 <div class="dodaj_post">
-                                    <form action="tablica.php" method="post">
                                         <input type="submit" name="dodaj" value="Dodaj post" class="zaloguj">
                                     </form>
                                 </div>
 
                             </div>
 
-                            <div class="y">
-                                <div class="autor">
-                                    <a href="#" class="link_autor">Mateusz</a>   10.11.2019 27.23
-                                </div>
-                                <div class="tekst">
-                                    asdf asdfasfas  as fsadfasdk fsa f sadsa fasfsf fa fdsfg sd g dsg sdf g dsfg sd g sd fg dsfgsdfg sdfgsdf g sdf fgsdg
-                                </div>  
-                            </div>
+                            <?php
 
-                            <div class="y">
-                            </div>
+                                 require_once "polaczenie.php";
+                                 mysqli_report(MYSQLI_REPORT_STRICT);
+                 
+                                 try
+                                 {
+                                     $conn = new mysqli($servername, $username, $password, $dbname);
+                                     if ($conn->connect_errno!=0)
+                                         {
+                                             throw new Exception(mysqli_connect_errno());
+                                         }
+                                     else
+                                     {
+                                         if($rezultat = $conn->query("SELECT TEKST,DATA,LOGIN FROM posty INNER JOIN uzytkownicy ON uzytkownicy.ID = posty.ID_LOGIN ORDER BY DATA DESC"))
+                                         {
+                                            if ($rezultat->num_rows > 0) 
+                                            {
+                                                while($wiersz = $rezultat->fetch_assoc())
+                                                {
+                                                    echo "<div class='y'>";
+                                                    echo "<div class='autor'>";
+                                                    echo "<a href='#' class='link_autor'>".$wiersz["LOGIN"]."</a>";
+                                                    echo "<div class='data'>".$wiersz["DATA"]."</div>";
+                                                    echo "</div>";
+                                                    echo "<div class='tekst'>".$wiersz["TEKST"]."</div>";
+                                                    echo "</div>";  
+                                                }
+                                            }
+                                            else 
+                                            {
+                                                    echo "<div class='y'>";
+                                                    echo "<div class='autor'>";
+                                                    echo "</div>";
+                                                    echo "<div class='tekst'>";
+                                                    echo "Brak postów do wyświetlenia";
+                                                    echo "</div>";  
+                                                    echo "</div>";
+                                            }
+                                         }
+                                         else
+                                         {
+                                             throw new Exception($conn->error);
+                                         }
+             
+                                         $conn->close();
+                                     }
+                                 }                  
+              
+                                 catch(Expection $e)
+                                 {
+                                     echo '<span style="color:red;"> Błąd połaczenia! Przepraszamy! Proszę spróbować za chwilę!</span>';
+                                 }
+             
 
-                            <div class="y">
-                            </div>
-
-                            <div class="y">
-                            </div>
-
-                            <div class="y">
-                            </div>
-
-                            <div class="y">
-                            </div>
+                            ?>
 
                             <div class="x">
                             </div>
                         </div>
                     </div>
-
 
                 </div>
         </body>
